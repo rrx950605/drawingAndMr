06ffbff66d3186d621c9be9a855dbe6563fc9ff2
rrx <rrx950605@gmail.com>
2018-05-08 14:18:27 +0800

    fix

--- src/main/java/com/bbkj/controller/yeelight.java
+++ src/main/java/com/bbkj/controller/yeelight.java
@@ -1,12 +1,17 @@
 package com.bbkj.controller;
 
+
 import com.mollin.yapi.YeelightDevice;
 
 public class yeelight {
     public static void main(String[] args) throws Exception {
+        //实例化灯
         YeelightDevice device = new YeelightDevice("192.168.1.47");
+        //开灯
         device.setPower(true);
+        //设置rgb颜色
         device.setRGB(255, 126, 0);
+        //设置亮度
         device.setBrightness(100);
     }
 }
